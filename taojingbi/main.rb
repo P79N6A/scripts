6ff03d732a54d7dc197b8721366a82c68413b215
chencheng <sorrycc@gmail.com>
2013-11-13 10:56:05 +0800

    update

--- taojingbi/main.rb
+++ taojingbi/main.rb
@@ -25,11 +25,10 @@ end
 _tb_token = page.search("input[name=_tb_token_]")[0].attr("value")
 page = agent.get "http://vip.taobao.com/home/grant_everyday_coin.htm?_tb_token_=#{_tb_token}"
 data = JSON.parse page.body
-coins_count = data["coinNew"] - data["coinOld"]
 
 # Logger.
 if data["isTake"] == "false"
-  p "#{@username} 领取淘金币 #{coins_count} 个"
+  p "#{@username} 领取淘金币 #{data["coinNew"] - data["coinOld"]} 个"
 else
   p "已领取，不能重复领取"
 end
