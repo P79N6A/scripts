741f403d4b803b26d65491be74e9d35e6d462f4d
chencheng <sorrycc@gmail.com>
2013-11-13 10:57:55 +0800

    update

--- taojingbi/main.rb
+++ taojingbi/main.rb
@@ -8,22 +8,20 @@ require "json"
 @password = ""
 
 agent = Mechanize.new
-page = agent.get "http://vip.taobao.com/vip_home.htm"
 
 # Login.
-unless page.body.match(/get-coin J_GetCoin/)
-  page = agent.get "https://login.taobao.com/member/login.jhtml"
-  form = page.form_with :id => "J_StaticForm"
-  form.TPL_username = @username
-  form.TPL_password = @password
-  agent.submit(form)
-  
-  page = agent.get "http://vip.taobao.com/vip_home.htm"
-end
+page              = agent.get "https://login.taobao.com/member/login.jhtml"
+form              = page.form_with :id => "J_StaticForm"
+form.TPL_username = @username
+form.TPL_password = @password
+agent.submit(form)
+
+# Get token.
+page  = agent.get "http://vip.taobao.com/vip_home.htm"
+token = page.search("input[name=_tb_token_]")[0].attr("value")
 
 # Get coins.
-_tb_token = page.search("input[name=_tb_token_]")[0].attr("value")
-page = agent.get "http://vip.taobao.com/home/grant_everyday_coin.htm?_tb_token_=#{_tb_token}"
+page = agent.get "http://vip.taobao.com/home/grant_everyday_coin.htm?_tb_token_=#{token}"
 data = JSON.parse page.body
 
 # Logger.
